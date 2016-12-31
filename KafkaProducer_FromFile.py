d63095aee486ee8998766e9b24d3cce7d973fadf
MokhFn <fnmokhtar@outlook.fr>
2016-12-31 23:40:51 +0100

    Update KafkaProducer_FromFile.py

--- KafkaProducer_FromFile.py
+++ KafkaProducer_FromFile.py
@@ -9,9 +9,10 @@ producer = KafkaProducer(bootstrap_servers='localhost:9092')
 
 for line in coordinates:
     parsed_line = line.split("(")[1].split(")")[0].split(", ")
-    print parsed_line
-    print b'LG '+parsed_line[1]+' LT '+parsed_line[0]+' PT '+str(datetime.datetime.now()).split('.')[0]
-    producer.send('test', b'LG '+parsed_line[1]+' LT '+parsed_line[0]+' PT '+str(datetime.datetime.now()).split('.')[0])
+    # Uncomment the two following lines for more output, and to keep track of what's actually happening
+    #print parsed_line
+    #print 'LG '+parsed_line[1]+' LT '+parsed_line[0]+' PT '+str(datetime.datetime.now()).split('.')[0]
+    producer.send('test', 'LG '+parsed_line[1]+' LT '+parsed_line[0]+' PT '+str(datetime.datetime.now()).split('.')[0])
     sleep(5)
 
 producer.flush()
