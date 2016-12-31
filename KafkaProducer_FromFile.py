45b4fc5062c10cdafb7addc4fa9e71e596aa6783
MokhFn <fnmokhtar@outlook.fr>
2016-12-31 22:13:42 +0100

    Update and rename KProducer_FromFile.py to KafkaProducer_FromFile.py

--- KafkaProducer_FromFile.py
+++ KafkaProducer_FromFile.py
@@ -1,7 +1,8 @@
 from kafka import KafkaProducer
 from time import sleep
+import datetime
 
-filename = "C:\Users\Mokh\Desktop\streaming_coordinates.txt"
+filename = "streaming_coordinates.txt"
 coordinates = open(filename).read().splitlines()
 
 producer = KafkaProducer(bootstrap_servers='localhost:9092')
@@ -9,9 +10,8 @@ producer = KafkaProducer(bootstrap_servers='localhost:9092')
 for line in coordinates:
     parsed_line = line.split("(")[1].split(")")[0].split(", ")
     print parsed_line
-    producer.send('test', b'LG '+parsed_line[1]+' LT '+parsed_line[0])
-    #sleep(5)
+    print b'LG '+parsed_line[1]+' LT '+parsed_line[0]+' PT '+str(datetime.datetime.now()).split('.')[0]
+    producer.send('test', b'LG '+parsed_line[1]+' LT '+parsed_line[0]+' PT '+str(datetime.datetime.now()).split('.')[0])
+    sleep(5)
 
 producer.flush()
-
-
