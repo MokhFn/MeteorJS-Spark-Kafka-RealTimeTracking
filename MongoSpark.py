c06d647d05f32bf7124c0b2669e459a4fa5c8deb
MokhFn <fnmokhtar@outlook.fr>
2016-12-31 22:09:32 +0100

    Update MongoSpark.py

--- MongoSpark.py
+++ MongoSpark.py
@@ -7,13 +7,14 @@ ssc = StreamingContext(sc, 1)
 kvs = KafkaUtils.createDirectStream(ssc, ["test"], {"bootstrap.servers": "localhost:9092"})
 lines = kvs.map(lambda x: x[1])
 coords = lines.map(lambda line: line.split(" "))
-data = coords.map(lambda coord: (float(coord[1]),float(coord[3]),str(datetime.datetime.now()).split('.')[0]))
-data.pprint(100000)
+data = coords.map(lambda coord: (float(coord[1]),float(coord[3]),str(" ".join(coord[5:7])),str(datetime.datetime.now()).split('.')[0]))
+data.pprint()
 def saveCoord(rdd):
     if (rdd.count() != 0) :
-        coord = sqlContext.createDataFrame(rdd, ["long", "latt", "timestamp"])
+        coord = sqlContext.createDataFrame(rdd, ["long", "lat", "produced_at" , "processed_at"])
+        print str(datetime.datetime.now()).split('.')[0]
         coord.write.format("com.mongodb.spark.sql").mode("append").save()
+        print str(datetime.datetime.now()).split('.')[0]
 data.foreachRDD(saveCoord)
 ssc.start()
 ssc.awaitTermination()
-#ssc.awaitTerminationOrTimeout(long)
